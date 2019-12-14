532b6b6e2b0e67a3c8a258149a460aade9722094
Igor Botian <igor.botian@al-enterprise.com>
2019-12-14 14:09:34 +0300

    Initial project structure

---
+++ cli/src/main/java/me/igorbotian/fczenit/ical/cli/App.java
@@ -0,0 +1,11 @@
+package me.igorbotian.fczenit.ical.cli;
+
+/**
+ * @author Igor Botian
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
