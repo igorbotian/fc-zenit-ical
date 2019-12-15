83091fa5818da78769b4eb06e075d49a10e7663d
Igor Botian <igor.botian@al-enterprise.com>
2019-12-15 12:51:41 +0300

    CLI application printing a fixed version of the FC Zenit calendar

--- cli/src/main/java/me/igorbotian/fczenit/ical/cli/App.java
+++ cli/src/main/java/me/igorbotian/fczenit/ical/cli/App.java
@@ -1,11 +1,24 @@
 package me.igorbotian.fczenit.ical.cli;
 
+import java.io.IOException;
+
+import me.igorbotian.fczenit.ical.core.FcZenitCalendar;
+import me.igorbotian.fczenit.ical.core.FcZenitCalendarEventParamFixer;
+import me.igorbotian.fczenit.ical.core.rfc2445.Rfc2445Calendar;
+import me.igorbotian.fczenit.ical.core.rfc2445.Rfc2445Param;
+
 /**
  * @author Igor Botian
  */
 public class App {
 
-    public static void main(String[] args) {
-        System.out.println("Hello world");
+    public static void main(String[] args) throws IOException {
+        Rfc2445Calendar cal = FcZenitCalendar.newInstance().downloadFromWebSite();
+        FcZenitCalendarEventParamFixer fixer = new FcZenitCalendarEventParamFixer();
+
+        for (Rfc2445Param param : cal.params()) {
+            Rfc2445Param fixed = fixer.fix(param);
+            System.out.println(fixed);
+        }
     }
 }
